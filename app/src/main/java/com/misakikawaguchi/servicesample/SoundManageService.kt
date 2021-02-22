2a6512593a6f075329381a5794183e271f15a002
川口美咲 <misaki@arcphilia.co.jp>
2021-02-22 23:42:45 +0900

    [Add]メディアプレーヤーフィールドを作成

--- app/src/main/java/com/misakikawaguchi/servicesample/SoundManageService.kt
+++ app/src/main/java/com/misakikawaguchi/servicesample/SoundManageService.kt
@@ -2,9 +2,13 @@ package com.misakikawaguchi.servicesample
 
 import android.app.Service
 import android.content.Intent
+import android.media.MediaPlayer
 import android.os.IBinder
 
 class SoundManageService : Service() {
+
+    // メディアプレーヤーフィールド
+    private var _player: MediaPlayer? = null
 
     override fun onBind(intent: Intent): IBinder {
         TODO("Return the communication channel to the service.")
