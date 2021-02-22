f69118b561d1be48023e5d03368990036d7e0220
川口美咲 <misaki@arcphilia.co.jp>
2021-02-23 01:13:17 +0900

    [Add]再生ボタン絵おタップした時の処理を追加

--- app/src/main/java/com/misakikawaguchi/servicesample/MainActivity.kt
+++ app/src/main/java/com/misakikawaguchi/servicesample/MainActivity.kt
@@ -1,11 +1,28 @@
 package com.misakikawaguchi.servicesample
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    // 再生ボタンをタップした時の処理
+    fun onPlayButtonClick(view: View) {
+        // インテントオブジェクトを生成
+        val intent = Intent(applicationContext, SoundManageService::class.java)
+        // サービスを起動
+        startService(intent)
+
+        // 再生ボタンをタップ不可に、停止ボタンをタップ可能にする
+        val btPlay = findViewById<Button>(R.id.btPlay)
+        val btStop = findViewById<Button>(R.id.btStop)
+        btPlay.isEnabled = false
+        btStop.isEnabled = true
     }
 }
