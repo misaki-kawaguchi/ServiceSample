d5f0a5440134025930a16936d8590a5bdf603580
川口美咲 <misaki@arcphilia.co.jp>
2021-02-22 23:48:51 +0900

    [Add]音声ファイルのURIオブジェクトを生成

--- app/src/main/java/com/misakikawaguchi/servicesample/SoundManageService.kt
+++ app/src/main/java/com/misakikawaguchi/servicesample/SoundManageService.kt
@@ -3,6 +3,7 @@ package com.misakikawaguchi.servicesample
 import android.app.Service
 import android.content.Intent
 import android.media.MediaPlayer
+import android.net.Uri
 import android.os.IBinder
 
 class SoundManageService : Service() {
@@ -17,5 +18,13 @@ class SoundManageService : Service() {
 
     override fun onBind(intent: Intent): IBinder {
         TODO("Return the communication channel to the service.")
+    }
+
+    // バックグラウンドで行う処理
+    override fun onStartCommand(intent: Intent, flags: Int, startId: Int): Int {
+        // 音声ファイルのURI文字列を作成
+        val mediaFileUriStr = "android.resource://${packageName}/${R.raw.mountain_stream}"
+        // 音声ファイルのURI文字列を元にURIオブジェクトを生成
+        val mediaFileUri = Uri.parse(mediaFileUriStr)
     }
 }
